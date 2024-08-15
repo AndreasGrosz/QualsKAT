b3dbc80a6ebe91ba6cc3f5cef710b61798a5d5cd
Andreas Gross <andreas.gross@swiss-ecommerce.ch>
2024-08-15 18:27:34 +0200

    240815-2 bug

--- main.py
+++ main.py
@@ -370,7 +370,7 @@ def main():
         if args.checkthis:
             models_to_process = get_models_for_task(config, 'check')
             for hf_name, short_name in models_to_process:
-                model_save_path = os.path.join(config['Paths']['models'], short_name)
+                model_save_path = os.path.join(config['Paths']['models'], hf_name.replace('/', '_'))
                 num_labels = len(categories)
                 model, tokenizer = get_model_and_tokenizer(hf_name, num_labels, categories, config)
 
